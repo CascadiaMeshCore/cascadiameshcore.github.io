e268db5b3b38b83e16af5f58fb445f19c139d570
Hude06 <judeallanhill@icloud.com>
2025-12-04 20:44:43 -0800

    Trigger Pages deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cascadiameshcore.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'CascadiaMeshCore', // Usually your GitHub org/user name.
+  projectName: 'cascadiameshcore.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // <-- set this to your main branch
 
   onBrokenLinks: 'throw',
 
